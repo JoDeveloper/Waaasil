6ae7e4fa26a676c74fa8ca8b47d73b70cfaf97a1
jodeveloper <jodeveloper8@gmail.com>
2019-10-28 09:07:40 +0200

    set debugShowCheckedModeBanner to false

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:waaasil/pages/waasil.dart';
 
-
-
 void main() {
-  runApp(MaterialApp(home: TravelHome()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TravelHome(),
+  ));
 }
